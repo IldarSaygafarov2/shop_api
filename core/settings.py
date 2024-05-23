b9c2312c15ada4a87311d90d0e85cb7c3cf1d8f8
sayildar <sayildar17@gmail.com>
2024-05-23 22:18:50 +0500

    added pagination

--- core/settings.py
+++ core/settings.py
@@ -108,6 +108,8 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = "ru"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
