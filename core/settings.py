d1d092348935f5ca01075b0cbce06227b855c268
sayildar <sayildar17@gmail.com>
2024-06-20 20:53:32 +0500

    added fixes

--- core/settings.py
+++ core/settings.py
@@ -93,10 +93,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = '/var/www/html/staticfiles'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
