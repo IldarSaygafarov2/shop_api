bc41ee5bfc96e882fdcb4716bfb42e923db9c26b
sayildar <sayildar17@gmail.com>
2024-06-20 10:35:05 +0500

    added fixes

--- core/settings.py
+++ core/settings.py
@@ -93,8 +93,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = "staticfiles/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
